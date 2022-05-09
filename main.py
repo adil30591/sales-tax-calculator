359ea5904d2f8e2d06a0193caeca5ee120dc3007
Adil Jamil <adil30591@gmail.com>
2022-05-10 01:26:55 +0200

    first commit

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-# This is a sample Python script.
+# This is a coding Task from Adil for Itemis.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Extract input from test file and put it in lines list
 
+def extract_input():
+    try:
+        lines = [line.strip() for line in open('test.txt', "r")]
+        print(lines)
+    except FileNotFoundError as e:
+        print('File Not Found', e)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# all tax calculation will happen here
+def tax_calculator():
 
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    extract_input()
